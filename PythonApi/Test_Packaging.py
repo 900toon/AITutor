0bd584353f67ad3588402cf2cfd90a0c71e37c05
900toon <900toon@gmail.com>
2025-07-09 23:09:12 +0800

    20250709

--- PythonApi/Test_Packaging.py
+++ PythonApi/Test_Packaging.py
@@ -193,12 +193,12 @@ os.environ['OPENAI_API_KEY'] = api_key
 
 client = OpenAI(api_key=api_key)
 
-original_model_name = "gpt-3.5-turbo"
+# original_model_name = "gpt-3.5-turbo"
 
 llm = ChatOpenAI(
     openai_api_key = api_key,
     temperature = 0,
-    model_name = "gpt-4o-mini-2024-07-18"
+    model_name = "gpt-4o"
 )
 
 temp = ConversationEntityMemory(llm = llm, k=10)
